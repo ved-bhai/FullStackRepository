511737c3c0f1b7a056ad458ad40097384729cb91
HP <HP@DESKTOP-E6G230H>
2025-01-11 00:59:15 -0800

    javaa

--- src/B.java
+++ src/B.java
@@ -3,7 +3,7 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world from b.java");
+		System.out.println("hello world from b.javaa");
 
 	}
 
